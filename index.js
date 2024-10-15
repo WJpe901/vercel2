4c0f42d2078f08785d5f4678830e04b7a4a19d3b
Wesley Condori Mozo <jupemo20009@hotmail.com>
2024-10-14 20:49:59 -0500

    about y contact

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ import { fileURLToPath } from "url";
 const app = express();
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-console.log(join(__dirname, "src/views"));
 
 app.set("views", join(__dirname, "src/views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => res.render("index"));
+app.get("/about", (req, res) => res.render("about"));
+app.get("/contact", (req, res) => res.render("contact"));
 
 app.listen(3000);
 console.log("server is listen on port", 3000);
